82111013bace2bfd634b79a6274ac97f677517bf
Sean <paulsean@nyayozangu.com>
2018-06-26 20:12:28 +0300

    1.1.4.11 search Changes: - fixed search
    Bug:
    - on submit disconnect network crashes app
    - all search not showing
    - showing old events
    still cant check if connection has internet
    app crashes on no network // no bundle
    app crashes on inconsistencies on recycler view
    on view post, desc shown on expand is not full
    when user doesn't exist comment crash

--- app/src/main/java/com/nyayozangu/labs/fursa/providers/MySuggestionProvider.java
+++ app/src/main/java/com/nyayozangu/labs/fursa/providers/MySuggestionProvider.java
@@ -8,7 +8,7 @@ import android.content.SearchRecentSuggestionsProvider;
 
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
     public final static String AUTHORITY =
-            "MySuggestionProvider";
+            "com.nyayozangu.labs.fursa.providers.MySuggestionProvider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
